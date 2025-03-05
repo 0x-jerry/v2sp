e1ec29b0a9423ff392051bef62af9641f8e51f41
Jerry <x.jerry.wang@gmail.com>
2025-03-05 03:00:42 +0000

    feat: implement encode/decode for sharing string

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,103 @@
-export const hello = 'world'
+import { isString } from '@0x-jerry/utils'
+import { URL } from 'node:url'
+
+export interface V2SPOption {
+  protocol: 'vmess' | 'vless'
+  uuid: string
+  host: string
+  port: number
+  name: string
+
+  transport?: {
+    type?: 'raw' | 'xhttp' | 'ws'
+
+    security?: 'none' | 'tls' | 'reality'
+
+    path?: string
+
+    host?: string
+
+    /**
+     * xhttp mode
+     *
+     * https://github.com/XTLS/Xray-core/pull/3994
+     */
+    mode?: 'auto' | 'packet-up' | 'stream-up'
+
+    /**
+     * xhttp extra
+     *
+     * https://github.com/XTLS/Xray-core/pull/4000
+     */
+    extra?: Record<string, unknown>
+  }
+}
+
+/**
+ *
+ * @param opt Only support a part of xray options
+ * @returns
+ */
+export function encode(opt: V2SPOption): string {
+  const { protocol, host, port, name, uuid, transport = {} } = opt
+
+  const url = new URL(`${protocol}://${host}:${port}`)
+
+  url.username = uuid
+  url.hash = name
+
+  Object.entries(transport).forEach(([key, value]) => {
+    const v = isString(value) ? value : JSON.stringify(value)
+    url.searchParams.set(key, v)
+  })
+
+  return url.toString()
+}
+
+export function decode(sharedString: string): V2SPOption {
+  const url = new URL(sharedString)
+
+  const $ = (key: string) => {
+    const val = url.searchParams.get(key)
+    if (!val) {
+      return undefined
+    }
+
+    return decodeURIComponent(val)
+  }
+
+  const config: V2SPOption = {
+    name: url.hash.slice(1),
+    protocol: url.protocol.slice(0, -1) as V2SPOption['protocol'],
+    host: url.hostname,
+    port: Number.parseInt(url.port),
+    uuid: url.username,
+  }
+
+  const keys: Array<keyof NonNullable<V2SPOption['transport']>> = [
+    'type',
+    'host',
+    'path',
+    'mode',
+    'security',
+    'extra',
+  ]
+
+  const entries = keys
+    .map((key) => {
+      let val = $(key)
+
+      if (key === 'extra' && val) {
+        val = JSON.parse(val)
+      }
+
+      return [key, val]
+    })
+    .filter(([_key, value]) => value != null)
+
+  if (entries.length) {
+    config.transport = Object.fromEntries(entries)
+  }
+
+  return config
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,49 @@
-describe('lib', () => {
-  it('should work', () => {
-    expect(1).toBe(1)
+import { decode, encode, type V2SPOption } from '../src'
+
+interface TestPair {
+  config: V2SPOption
+  sharedStr: string
+}
+
+describe('v2sp', () => {
+  const tests: TestPair[] = [
+    {
+      config: {
+        protocol: 'vmess',
+        uuid: '99c80931-f3f1-4f84-bffd-6eed6030f53d',
+        host: 'qv2ray.net',
+        port: 31415,
+        name: 'VMessTCPNaked',
+      },
+      sharedStr: 'vmess://99c80931-f3f1-4f84-bffd-6eed6030f53d@qv2ray.net:31415#VMessTCPNaked',
+    },
+    {
+      config: {
+        protocol: 'vmess',
+        uuid: '44efe52b-e143-46b5-a9e7-aadbfd77eb9c',
+        host: 'qv2ray.net',
+        port: 6939,
+        name: 'VMessWebSocketTLS',
+        transport: {
+          type: 'ws',
+          host: 'qv2ray.net',
+          path: '/somewhere',
+        },
+      },
+      sharedStr:
+        'vmess://44efe52b-e143-46b5-a9e7-aadbfd77eb9c@qv2ray.net:6939?type=ws&host=qv2ray.net&path=%2Fsomewhere#VMessWebSocketTLS',
+    },
+  ]
+
+  it('#encode', () => {
+    for (const test of tests) {
+      expect(encode(test.config), test.config.name).toBe(test.sharedStr)
+    }
+  })
+
+  it('#decode', () => {
+    for (const test of tests) {
+      expect(decode(test.sharedStr), test.config.name).toEqual(test.config)
+    }
   })
 })
